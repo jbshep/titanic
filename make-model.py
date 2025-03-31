40e4e5966031e65a8a8834b7040e75672add62bb
Jason Shepherd <jbshep@gmail.com>
2025-03-31 09:45:28 -0500

    more progress

--- make-model.py
+++ make-model.py
@@ -17,3 +17,9 @@ df = df[categorical_features + [target]].dropna()
 
 X = df[categorical_features]
 y = df[target]
+
+encoder = OneHotEncoder(drop='first', sparse_output=False)
+X_encoded = encoder.fit_transform(X)
+X_encoded = pd.DataFrame(X_encoded, columns=encoder.get_feature_names_out(categorical_features))
+
+X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.2, random_state=42)
